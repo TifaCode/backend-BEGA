d5f68f5a7d894dcaac2d84f3b625620d5a5c204d
OksanaMesbahi <117586582+OksanaMesbahi@users.noreply.github.com>
2023-05-15 10:28:31 +0200

    events models

---
+++ models/events.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const eventsSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  date: Date,
+  lieu: {type: String, required: true},
+  description: {type: String, required: true},
+  participants: [{
+    type: mongoose.Types.ObjectId, ref: 'users'
+  }],
+  status: Boolean,
+  created_at: Date,
+  depenseId: [{
+    type: mongoose.Types.ObjectId, ref: 'depenses'
+  }],
+  todoId: [{
+    type: mongoose.Types.ObjectId, ref: 'todos'
+  }],
+  cagnotteId: {
+    type: mongoose.Types.ObjectId, ref: 'cagnottes'
+  },
+});
+
+const Event = mongoose.model('events', eventsSchema);
+
+module.exports = Event;

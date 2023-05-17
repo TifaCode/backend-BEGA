f75bde810d4723b30439e2b5bb4dafecd7728033
vincent <vincent.bickel@gmail.com>
2023-05-17 14:34:58 +0200

    todo

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,16 @@
 var express = require("express");
 var router = express.Router();
 const authentication = require("../middleware/authentification");
-const { addTodo } = require("../controller/todoController");
+const {
+  addTodo,
+  updateTodo,
+  deleteTodo,
+  getAllTodo,
+} = require("../controller/todoController");
 
 router.post("/addtodo", addTodo);
+router.put("/updatetodo", updateTodo);
+router.delete("/deletetodo", deleteTodo);
+router.get("/getalltodo/:eventId", getAllTodo);
 
 module.exports = router;

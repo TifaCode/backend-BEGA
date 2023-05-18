476694481e700070082eab50d6d8cb3e9f4fc6f0
OksanaMesbahi <117586582+OksanaMesbahi@users.noreply.github.com>
2023-05-18 12:19:49 +0200

    update event info

--- routes/strongbox.js
+++ routes/strongbox.js
@@ -4,11 +4,9 @@ const authentication = require("../middleware/authentification");
 const {
   createStrongbox,
   getStrongboxByEvent,
-  deleteStrongbox,
 } = require("../controller/strongboxController");
 
 router.post("/createstrongbox", createStrongbox); //req.body.creatorId, req.body.eventId;
 router.get("/getstrongbox/:eventId", getStrongboxByEvent); // req.body.eventId
-router.delete("/deletestrongbox", deleteStrongbox); //req.body.strongboxId;
 
 module.exports = router;

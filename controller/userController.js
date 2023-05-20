1c0515f84fa2d6f94f761f54e34b472d91028b12
vincent <vincent.bickel@gmail.com>
2023-05-20 15:39:30 +0200

    test

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,8 @@
 const User = require("../models/userModel");
 const { checkBody } = require("../middleware/checkBody");
 const bcrypt = require("bcrypt");
+const sendEmail = require("../middleware/sendEmail");
+const { v4: uuidv4 } = require("uuid");
 
 ////////////////////SIGNUP////////////////////////////////////////////////////
 const signUpUser = async (req, res) => {
@@ -86,5 +88,47 @@ const updateProfil = async (req, res) => {
     res.json({ result: true, error: "Porfil modifié" });
   }
 };
-
-module.exports = { signUpUser, signInUser, logout, userProfil, updateProfil };
+/////////////////////////////////////Reset password////////////////////////////
+const emailSentToresetPassword = async (req, res) => {
+  const { email } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) res.json({ result: false, error: "pas d'utilisateur" });
+    else {
+      const link = `http://localhost:3001/mailresetpassword/${user._id}`; //urlfront
+      sendEmail("yota68@msn.com", "reset your password", link);
+      res.json({
+        result: true,
+        error: "a link sent to reset your email password",
+      });
+    }
+  } catch (error) {
+    res.json({ result: false, error: "Impossible" });
+  }
+};
+/////////////////le front doit récuprer le params de l'url et le mettre dans l'url
+const resetPasswordFromEmail = async (req, res) => {
+  const { userId } = req.params;
+  const { password } = req.body;
+  let newPassword = await bcrypt.hash(req.body.password, 5);
+  try {
+    const user = await User.findById(userId);
+    if (!user) res.json({ result: false, error: "impossible a modifier" });
+    else {
+      await user.updateOne({ password: newPassword });
+      res.json({ result: true, error: "modified" });
+    }
+  } catch (error) {
+    res.json({ resul: false, error: "Impossible" });
+  }
+};
+///////////////////////////////////////////////////////////////////////////
+module.exports = {
+  signUpUser,
+  signInUser,
+  logout,
+  userProfil,
+  updateProfil,
+  emailSentToresetPassword,
+  resetPasswordFromEmail,
+};

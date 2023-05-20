131dedeafb53d31428ec7b9dfb647402d22e704b
vincent <vincent.bickel@gmail.com>
2023-05-20 15:32:24 +0200

    reset password and send email

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,8 @@ const {
   logout,
   userProfil,
   updateProfil,
+  emailSentToresetPassword,
+  resetPasswordFromEmail,
 } = require("../controller/userController");
 
 /* post users listing. */
@@ -15,5 +17,7 @@ router.post("/signin", signInUser); //req.body.email;
 router.post("/logout", authentication, logout); //req.user.authTokens;
 router.get("/userprofil", authentication, userProfil); //req.user;
 router.post("/updateprofil", authentication, updateProfil); // req.body.userId, req.bodyfirstname, req.bodylastname, req.body.email, req.body.password
+router.post("/resetpassword", emailSentToresetPassword); // req.body.email, envoi email
+router.post("/mailresetpassword/:userId", resetPasswordFromEmail); // req.body.password, reset password
 
 module.exports = router;
